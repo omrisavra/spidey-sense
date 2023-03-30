ec931a0d9c2eb576df3defaaf629a15744361ac0
manorelad <95222201+manorelad@users.noreply.github.com>
2023-03-31 00:58:19 +0300

    Add vibrate when alerting  (#6)
    * Add semantic ui
    
    * Add vibrate
    
    ---------
    
    Co-authored-by: eladmanor <eladma@rewire.to>

--- alert_device/src/App.tsx
+++ alert_device/src/App.tsx
@@ -3,30 +3,25 @@ import './App.css';
 import axios from 'axios';
 import styled from 'styled-components';
 
-const serverAddress = 'http://localhost:3001/notify';
+const serverAddress = 'https://d82d-31-154-67-98.eu.ngrok.io/notify';
 
-const StyledAlert = styled.div<{isAlerted: boolean}>`
+const StyledAlert = styled.div<{ isAlerted: boolean }>`
   height: 100vh;
-  background-color: ${({isAlerted}) => isAlerted ? "red" : "green"};
-  display: flex;  
-  justify-content: center; 
+  background-color: ${({ isAlerted }) => (isAlerted ? 'red' : 'green')};
+  display: flex;
+  justify-content: center;
   align-items: center;
-`
+`;
 
-const StyledText = styled.h1`
-  
-`
+const StyledText = styled.h1``;
 
-const AlertComponent: React.FC<{isAlerted: boolean}> = ({isAlerted}) => {
-  useEffect(() => {
-    if (!isAlerted) return;
-    console.log("sending alert")
-    navigator.vibrate([
-      100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
-    ]);
-  }, [isAlerted])
-  return <StyledAlert isAlerted={isAlerted}><StyledText>{isAlerted ? "alertt!!!!!": "okkk!"}</StyledText></StyledAlert>
-}
+const AlertComponent: React.FC<{ isAlerted: boolean }> = ({ isAlerted }) => {
+  return (
+    <StyledAlert isAlerted={isAlerted}>
+      <StyledText>{isAlerted ? 'alertt!!!!!' : 'okkk!'}</StyledText>
+    </StyledAlert>
+  );
+};
 
 const App = () => {
   const [isAlerted, setIsAlerted] = useState(false);
@@ -34,12 +29,27 @@ const App = () => {
   useEffect(() => {
     setInterval(async () => {
       const notifyServerResponse = await axios.get(serverAddress);
-      const alert = notifyServerResponse.data === "true";
+      const alert = notifyServerResponse.data === 'true';
       setIsAlerted(alert);
     }, 100);
-  }, [])
+  }, []);
 
-  return <AlertComponent isAlerted={isAlerted}/>;
+  useEffect(() => {
+    if (isAlerted) {
+      setTimeout(() => {
+        navigator.vibrate([
+          100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100,
+          30, 100,
+        ]);
+      }, 50);
+    }
+  }, [isAlerted]);
+
+  return (
+    <>
+      <AlertComponent isAlerted={isAlerted} />
+    </>
+  );
 };
 
 export default App;
