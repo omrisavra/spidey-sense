b5edb38cd6e118f4b9e9514a4db65df8712f762a
Itamar <itamar.holtzman@gmail.com>
2023-03-30 12:40:36 +0300

    Improve motor API

--- alert_device/notify_users/vibration_notificiation/vibration_motor.py
+++ alert_device/notify_users/vibration_notificiation/vibration_motor.py
@@ -5,6 +5,7 @@ import logging
 class VibrationMotor(object):
     def __init__(self, gpio_pin_id):
         self._gpio_pin_id = gpio_pin_id
+        self._is_running = False
         self.logger = logging.getLogger(str(self))
         self.logger.debug("Setting up PI...")
         self.logger.debug("Setting up device mode as BCM")
@@ -13,13 +14,25 @@ class VibrationMotor(object):
         self.logger.debug(f"Setting up pin {self._gpio_pin_id} as output")
         GPIO.setup(gpio_pin_id, GPIO.OUT)
 
-    def start_vibration(self):
-        self.logger.debug("Starting vibration")
-        GPIO.output(self._gpio_pin_id, GPIO.HIGH)
+    def start(self):
+        if not self._is_running:
+            self.logger.debug("Starting motor")
+            GPIO.output(self._gpio_pin_id, GPIO.HIGH)
+            self._is_running = True
+        else:
+            self.logger.debug("Tried to start while motor was already running")
 
-    def stop_vibration(self):
-        self.logger.debug("Stopping vibration")
-        GPIO.output(self._gpio_pin_id, GPIO.LOW)
+    def stop(self):
+        if self._is_running:
+            self.logger.debug("Stopping motor")
+            GPIO.output(self._gpio_pin_id, GPIO.LOW)
+            self._is_running = False
+        else:
+            self.logger.debug("Tried to stop when motor wasn't running")
+
+    @property
+    def is_running(self):
+        return self._is_running
 
     def __repr__(self):
         return f"<VibrationMotor:{self._gpio_pin_id}>"
