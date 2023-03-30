01faedbdc772f49871d3d42fd0096009a5b9c42c
omrisavra <omri@komodor.io>
2023-03-31 02:24:49 +0300

    images broadcasting

--- alert_device/src/App.tsx
+++ alert_device/src/App.tsx
@@ -1,45 +1,75 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import axios from 'axios';
-import styled from 'styled-components';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import axios from "axios";
+import styled from "styled-components";
 
-const serverAddress = 'http://10.10.10.49:3001/notify';
+const serverAddress = "http://10.10.10.49:3001";
 
-const StyledAlert = styled.div<{isAlerted: boolean}>`
+const StyledAlert = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+const StyledText = styled.h1``;
+
+const StyledApp = styled.div<{ isAlerted: boolean }>`
   height: 100vh;
-  background-color: ${({isAlerted}) => isAlerted ? "red" : "green"};
-  display: flex;  
-  justify-content: center; 
+  width: 100vw;
+  background-color: ${({ isAlerted }) => (isAlerted ? "red" : "green")};
+  display: flex;
+  flex-direction: column;
   align-items: center;
 `
 
-const StyledText = styled.h1`
-  
+
+//create your forceUpdate hook
+function useForceUpdate(){
+  const [, setValue] = useState(0); // integer state
+  return () => setValue(value => value + 1); // update state to force render
+  // A function that increment 👆🏻 the previous state like here 
+  // is better than directly setting `setValue(value + 1)`
+}
+
+const StyledImage = styled.img`
+  filter: hue-rotate(-45deg);
+
 `
 
-const AlertComponent: React.FC<{isAlerted: boolean}> = ({isAlerted}) => {
+const AttackerFeed: React.FC<{isAlerted: boolean}> = ({isAlerted}) => {
+  const forceUpdate = useForceUpdate();
   useEffect(() => {
-    if (!isAlerted) return;
-    console.log("sending alert")
-    navigator.vibrate([
-      100, 30, 100, 30, 100, 30, 200, 30, 200, 30, 200, 30, 100, 30, 100, 30, 100,
-    ]);
+    forceUpdate()
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [isAlerted])
-  return <StyledAlert isAlerted={isAlerted}><StyledText>{isAlerted ? "alertt!!!!!": "okkk!"}</StyledText></StyledAlert>
+  return <StyledImage src={serverAddress + "/img"} alt="Image" />
 }
+
+const AlertComponent: React.FC<{ alertText: string }> = ({ alertText }) => {
+  return (
+    <StyledAlert>
+      <StyledText>{alertText}</StyledText>
+    </StyledAlert>
+  );
+};
 
 const App = () => {
   const [isAlerted, setIsAlerted] = useState(false);
 
   useEffect(() => {
     setInterval(async () => {
-      const notifyServerResponse = await axios.get(serverAddress);
+      const notifyServerResponse = await axios.get(serverAddress + "/notify");
       const alert = notifyServerResponse.data === "true";
       setIsAlerted(alert);
     }, 100);
-  }, [])
+  }, []);
 
-  return <AlertComponent isAlerted={isAlerted}/>;
+  return (
+    <StyledApp isAlerted={isAlerted}>
+      <AlertComponent alertText={isAlerted ? "Alertttt": "Ok!!!!"} />
+      {isAlerted && <AttackerFeed isAlerted={isAlerted}/>}
+    </StyledApp>
+  );
 };
 
 export default App;
--- server/src/app.ts
+++ server/src/app.ts
@@ -1,5 +1,6 @@
 import { createServer, plugins } from 'restify';
 import corsMiddleware from 'restify-cors-middleware';
+import fs from "fs"
 
 const cors = corsMiddleware({
   origins: ['*'],
@@ -11,10 +12,12 @@ const app = createServer();
 app.pre(cors.preflight);
 app.use(cors.actual);
 app.use(plugins.jsonBodyParser());
+app.use(plugins.queryParser());
 
 const port = 3001;
 
 let isNotified = false;
+let lastImage: Buffer | null = null;
 
 app.get('/notify', (req, res, next) => {
     res.send(isNotified ? "true" : "false")
@@ -26,6 +29,34 @@ app.post('/notify', (req, res, next) => {
     res.send("sent")
     next()
 })
+
+app.post('/img', (req, res, next) => {
+  res.send("got image");
+
+  console.log(`got image req `, {req});
+  console.log(`got image body`, req.body);
+
+  lastImage = Buffer.from(req.body.data, 'base64');
+
+  fs.writeFileSync('image.jpg', lastImage);
+
+  next();
+});
+
+app.get('/img', (req, res, next) => {
+  fs.readFile('./image.jpg', function(err, file) {
+    if (err) {
+      res.send(500);
+      return next();
+    }
+
+    res.write(file);
+    res.end();
+    console.log("sent response")
+    return next()
+  })
+})
+
 
 app.listen(port, function () {
   console.log(`server started and listening on ${port}`);
