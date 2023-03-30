c486598b293013d4b9a6f0c2ceaa100f94b9f945
manorelad <95222201+manorelad@users.noreply.github.com>
2023-03-30 18:14:55 +0300

    Add axios to reat reqeusts (#1)
    Co-authored-by: eladmanor <eladma@rewire.to>

--- alert_device/src/App.tsx
+++ alert_device/src/App.tsx
@@ -1,24 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import './App.css';
-
-const serverAddress = "http://localhost:3001/notify";
+import * as axios from 'axios';
+const serverAddress = 'http://localhost:3001/notify';
 
 const App = () => {
   const [isAlerted, setIsAlerted] = useState(false);
 
   useEffect(() => {
     setInterval(async () => {
-      const notifyServerResponse = await fetch(serverAddress);
-      console.log(notifyServerResponse.body)
-      setIsAlerted(true)
-    }, 1_000)
-  }, [])
+      const notifyServerResponse = await axios.default.get(serverAddress);
+      console.log(notifyServerResponse);
+      setIsAlerted(true);
+    }, 1_000);
+  }, []);
 
-  return (
-    <div className="App">
-      {isAlerted ? "Alert!!!!!!": "Ok"}
-    </div>
-  );
-}
+  return <div className='App'>{isAlerted ? 'Alert!!!!!!' : 'Ok'}</div>;
+};
 
 export default App;
