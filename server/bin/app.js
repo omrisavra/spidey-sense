1f5398f734e05fcb2ed11b2076517f02a9f656c8
omrisavra <omri@komodor.io>
2023-03-30 20:54:02 +0300

    fix

--- server/bin/app.js
+++ server/bin/app.js
@@ -6,9 +6,9 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const restify_1 = require("restify");
 const restify_cors_middleware_1 = __importDefault(require("restify-cors-middleware"));
 const cors = (0, restify_cors_middleware_1.default)({
-    origins: ["*"],
-    allowHeaders: ["*"],
-    exposeHeaders: ["*"],
+    origins: ['*'],
+    allowHeaders: ['*'],
+    exposeHeaders: ['*'],
 });
 const app = (0, restify_1.createServer)();
 app.pre(cors.preflight);
